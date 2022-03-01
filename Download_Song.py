5c6831f41ec0409c996f5da8da16e8fa40024027
codecube <fabiofiorucci6@gmail.com>
2022-03-01 18:27:44 +0100

    reformat code

--- Download_Song.py
+++ Download_Song.py
@@ -1,12 +1,13 @@
 from pytube import YouTube
+
 file = open('song.txt', 'r')
 for line in file:
-   url = line
-   try:
-       yt = YouTube(url)
-       streams = yt.streams.filter(only_audio=True)
-       stream = yt.streams.get_by_itag(140)
-       stream.download()
-   except:
-       pass
+    url = line
+    try:
+        yt = YouTube(url)
+        streams = yt.streams.filter(only_audio=True)
+        stream = yt.streams.get_by_itag(140)
+        stream.download()
+    except:
+        pass
 file.close()
